d5c056e75ab2d4ed813bf1e1473f9cfc9d601235
23A9CQ61065 <147416527+23A9CQ61065@users.noreply.github.com>
2024-01-03 12:35:49 +0530

    #include<stdio.h> int main(){     int n,i,sum=0,c=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++){         scanf("%d",&arr[i]);         sum+=arr[i];     }     int avg=sum/n;     for(i=0;i<n;i++){         if(avg>=arr[i]) c++;}         printf("%d",c); }

---
+++ Elements_smaller_than_average.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n,i,sum=0,c=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+        sum+=arr[i];
+    }
+    int avg=sum/n;
+    for(i=0;i<n;i++){
+        if(avg>=arr[i]) c++;}
+        printf("%d",c);
+}
